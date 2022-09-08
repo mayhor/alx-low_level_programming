a55e478d607e6f5240c7d652c2bb9b8398b89277
mayhor <mawodire@gmail.com>
2022-09-08 10:11:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,13 @@
 
 /**
  * main -  prints exactly "Programming is like building a multilingual puzzle.
-
  *
-
  * Return: Always 0 (Success)
-**/
+*/
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
-
-           
