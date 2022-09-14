67daccee872d35d6c7f9a6096e59e428f7c7f267
mayhor <mawodire@gmail.com>
2022-09-13 17:13:32 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints sum of multipliers of 3 or 5 below 1024.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i <= 1023; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
