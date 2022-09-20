49ea1c69e505132276d64140a15d0e8ada9aa3ea
mayhor <mawodire@gmail.com>
2022-09-20 06:28:28 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,18 +3,18 @@
 
 /**
  * puts_half - prints half of a string followed by a new line
- * @s: to be halfed
+ * @str: to be halfed
  */
 
 void puts_half(char *str)
 {
 	int i, len, half;
 	
-	len = strlen(s);
+	len = strlen(str);
 	half = (len - 1) / 2;
 	for (i = half + 1; i < len; i++)
 	{
-		_putchar(*(s + i));
+		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
