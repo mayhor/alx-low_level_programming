c2800e4412d9565e7fb04806cc843fbe404c8a33
mayhor <mawodire@gmail.com>
2022-09-22 10:15:49 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int a;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		a = -a;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+	if (a / 10)
+		print_number(a / 10);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((a % 10) + '0');
 }
