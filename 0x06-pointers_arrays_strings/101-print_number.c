5115cfe53b468403a4ebce3fdfef194c8d45d80f
mayhor <mawodire@gmail.com>
2022-09-22 10:01:18 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int i;
 
 	if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		i = -i;
 	}
 
 	if (i / 10)
-	{
 		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+
+	_putchar((i % 10) + '0');
 }
