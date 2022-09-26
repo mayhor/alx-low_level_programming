f3ff11e806f8386967de18da2050b67ddc3cf36b
mayhor <mawodire@gmail.com>
2022-09-26 15:25:56 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - lenght of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	strspn(s, accept);
+	return (0);
+}
