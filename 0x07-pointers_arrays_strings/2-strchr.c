efad540a09a31812d4e6b5881abdfa2862a8f699
mayhor <mawodire@gmail.com>
2022-09-26 14:55:42 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input
+ * @c: input
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (0);
+}
