c904678089db47b50c12d1b14602c400358f321f
mayhor <mawodire@gmail.com>
2022-09-28 18:18:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,11 +12,11 @@ int is_prime_number(int n)
 
 	i = 2;
 	if (n % 1 == 0)
-		return 0;
+		return (0);
 	else if (n == 1)
-		return 0;
+		return (0);
 	else if (n < 0)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 }
