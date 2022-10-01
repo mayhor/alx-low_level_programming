5b72f684ecd9d8e97c0fa42daacaebe961b1936b
mayhor <mawodire@gmail.com>
2022-09-30 18:26:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
