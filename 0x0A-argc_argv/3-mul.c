3af51c654aeadeb73e9abfa76833ab97d2373d17
mayhor <mawodire@gmail.com>
2022-09-30 18:41:04 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, res = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res *= atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
