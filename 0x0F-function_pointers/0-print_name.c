6ba3e359b192080eaa1121f0c7f6118ceded24a1
Awodire Mayowa <mawodire@gmail.com>
2022-10-13 14:23:56 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,14 +7,14 @@
  * @name: name of the person
  * @f: is pointer
  * Return: Nothing.
-**/
+ */
 
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	{
-		return (0);
+		return;
 	}
 	else
 	{
