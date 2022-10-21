b198cd6a5cc2d5f18d88c5a6d4936e14f4be285c
Awodire Mayowa <mawodire@gmail.com>
2022-10-21 01:13:21 +0100

    Linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@
 
 size_t list_len(const list_t *h)
 {
-	const list_t *temp;
-	unsigned int counter = 0;
-	temp = h;
-	while (temp)
-	{
-		counter++;
-		temp = temp->next;
-	}
-	return (counter);
+const list_t *temp;
+unsigned int counter = 0;
+temp = h;
+while (temp)
+{
+counter++;
+temp = temp->next;
+}
+return (counter);
 }
