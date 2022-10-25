c21ee495b361909469d96edfc440315f3f695ddd
Awodire Mayowa <mawodire@gmail.com>
2022-10-25 08:58:43 +0100

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function that deletes the head node
+ * @head: head node
+ * Return: head node’s data
+ */
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new = *head;
+	int num;
+
+	if (!*head)
+		return (0);
+
+	num = (*head)->n;
+	*head = new->next;
+	free(new);
+	return (num);
+}
